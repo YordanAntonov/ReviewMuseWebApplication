c9e40b303c1780d495a373bdb1319387a5cc6ea6
Yordan Antonov <dankata168@gmail.com>
2023-07-09 16:02:06 +0300

    Initial Commit
    Creating project architecture.

---
+++ ReviewMuse.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ReviewMuse.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
