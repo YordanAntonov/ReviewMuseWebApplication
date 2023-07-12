a6c757a3dca0bcafd046ad222c459ec5cbcd1423
Yordan Antonov <dankata168@gmail.com>
2023-07-12 14:43:36 +0300

    Added config for AuthorsBooks mapping table!

---
+++ ReviewMuse.Data/EntityConfigurations/AuthorsBooksConfiguration.cs
@@ -0,0 +1,28 @@
+﻿namespace ReviewMuse.Data.EntityConfigurations
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+    using ReviewMuse.Data.Models.MappingTables;
+
+    public class AuthorsBooksConfiguration : IEntityTypeConfiguration<AuthorsBooks>
+    {
+        public void Configure(EntityTypeBuilder<AuthorsBooks> builder)
+        {
+            builder
+                .HasKey(e => new { e.AuthorId, e.BookId });
+
+            builder
+                .HasOne(a => a.Author)
+                .WithMany(a => a.AuthorBooks)
+                .HasForeignKey(a => a.AuthorId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder
+                .HasOne(b => b.Book)
+                .WithMany(b => b.BookAuthors)
+                .HasForeignKey(b => b.BookId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
