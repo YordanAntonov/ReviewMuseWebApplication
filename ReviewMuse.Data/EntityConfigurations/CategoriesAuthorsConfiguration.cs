53e450be91b404c3ea975cb30dfe467b18b450b8
Yordan Antonov <dankata168@gmail.com>
2023-07-12 14:52:00 +0300

    Addded CategoriesAuthors mapping table configuration!

---
+++ ReviewMuse.Data/EntityConfigurations/CategoriesAuthorsConfiguration.cs
@@ -0,0 +1,29 @@
+﻿namespace ReviewMuse.Data.EntityConfigurations
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+    using ReviewMuse.Data.Models.MappingTables;
+
+    public class CategoriesAuthorsConfiguration : IEntityTypeConfiguration<CategoriesAuthors>
+    {
+        public void Configure(EntityTypeBuilder<CategoriesAuthors> builder)
+        {
+            builder
+                .HasKey(entity => new { entity.CategoryId, entity.AuthorId });
+
+            builder
+                .HasOne(a => a.Author)
+                .WithMany(a => a.AuthorCategories)
+                .HasForeignKey(a => a.AuthorId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder
+                .HasOne(c => c.Category)
+                .WithMany(c => c.AuthorsCategory)
+                .HasForeignKey(c => c.CategoryId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+        }
+    }
+}
