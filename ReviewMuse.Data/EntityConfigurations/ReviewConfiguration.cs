2a7f9ace4e00cb40ba197b675770b466e95f9b5b
Yordan Antonov <dankata168@gmail.com>
2023-07-12 15:26:13 +0300

    Added  Review Config!

---
+++ ReviewMuse.Data/EntityConfigurations/ReviewConfiguration.cs
@@ -0,0 +1,25 @@
+﻿namespace ReviewMuse.Data.EntityConfigurations
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+    using ReviewMuse.Data.Models;
+
+    public class ReviewConfiguration : IEntityTypeConfiguration<Review>
+    {
+        public void Configure(EntityTypeBuilder<Review> builder)
+        {
+            builder
+                .HasOne(u => u.User)
+                .WithMany(u => u.Reviews)
+                .HasForeignKey(u => u.UserId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder
+                .HasOne(b => b.Book)
+                .WithMany(b => b.Reviews)
+                .HasForeignKey(b => b.BookId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
