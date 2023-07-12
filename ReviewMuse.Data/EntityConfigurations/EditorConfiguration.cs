8aaa3b0039b9c2e197522de8bb1b6e6792fc6178
Yordan Antonov <dankata168@gmail.com>
2023-07-12 15:28:37 +0300

    Added Edtor Config!

---
+++ ReviewMuse.Data/EntityConfigurations/EditorConfiguration.cs
@@ -0,0 +1,19 @@
+﻿namespace ReviewMuse.Data.EntityConfigurations
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+    using ReviewMuse.Data.Models;
+
+    public class EditorConfiguration : IEntityTypeConfiguration<Editor>
+    {
+        public void Configure(EntityTypeBuilder<Editor> builder)
+        {
+            builder
+                .HasOne(u => u.User)
+                .WithMany(u => u.Editors)
+                .HasForeignKey(u => u.UserId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
